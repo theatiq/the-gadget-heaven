e2a88c96538bff0a1b0a71aa1d1e41a8ea613fc9
theatiq <theatiq@gmail.com>
2024-11-05 15:09:03 +0600

    Modals added

--- src/pages/Carts.jsx
+++ src/pages/Carts.jsx
@@ -1,9 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { getAllCarts, removeCart } from "../utils/uutility";
 import Cart from "../pages/Cart";
+import Modal from "react-modal";
+import payment from "../assets/Group.png";
+import "../index.css";
+
+Modal.setAppElement("#root");
 
 const Carts = () => {
   const [gadgets, setGadgets] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
   useEffect(() => {
     const carts = getAllCarts();
     setGadgets(carts);
@@ -22,6 +32,30 @@ const Carts = () => {
 
   return (
     <div>
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={closeModal}
+        contentLabel="My Modal"
+        className={"modal-box"}
+        overlayClassName={"modal-overlay"}
+      >
+        <div className="flex flex-col items-center gap-2">
+          <img src={payment} alt="paid" />
+          <h3 className="text-3xl font-bold">Payment Successful</h3>
+          <p className="text-xl">Thanks for Purchasing</p>
+          <p className="text-xl">
+            Total:{" "}
+            {gadgets.reduce((a, b) => {
+              return a + b.price;
+            }, 0)}{" "}
+          </p>
+          <div className="modal-action">
+            <button onClick={closeModal} className="btn w-full">
+              Close
+            </button>
+          </div>
+        </div>
+      </Modal>
       <div className="flex justify-between my-5">
         <h1 className="text-xl font-bold">Cart</h1>
         <div className="flex justify-between items-center gap-5">
@@ -37,7 +71,10 @@ const Carts = () => {
           >
             Sort by Price
           </button>
-          <button className="btn border bg-purple-500 text-white rounded-3xl w-[120px]">
+          <button
+            onClick={openModal}
+            className="btn border bg-purple-500 text-white rounded-3xl w-[120px]"
+          >
             Purchase
           </button>
         </div>
